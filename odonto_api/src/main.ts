fb9b7462241a9f6aec6ded29c01eb61ac0c7292e
Anderson Rodrigues <eusouanderson@outlook.com>
2025-02-06 11:59:06 -0300

    App Controller - Server is running

--- odonto_api/src/main.ts
+++ odonto_api/src/main.ts
@@ -5,5 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-console.log("http://localhost:3000");
+
 bootstrap();
+export default bootstrap;
